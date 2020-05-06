a4db7153cda35e70b9705d389dd0d9f19fe5b65b
Yvette <nchombuayvta@gmil.com>
2020-05-06 18:50:36 +0200

    "more changes'

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
  //exercise 11
- function createLink(){
- let navP = document.querySelector('.nav')
+ /*window.onload = function createLink(){
+ let navP = document.querySelector('nav:last-child')
  let newA = document.createElement('a')
  newA.setAttribute = ('href', '#')
- alert.classList.add ('p-2 text-muted')
+ newA.classList.add ('p-2 text-muted')
  newA.innerText = 'fashion'
- navP[0].appendChild(newA);
+ newA.style.backgroundColor = 'red'
+ navP.appendChild(newA);*/
 
 }
+ function to add a new link into the navbar
+       function addLinkToNav() {
+           let navs = document.querySelector('nav:last-child');
+           let theLink = document.createElement('a');
+             theLink.innerHTML = 'AddedLink';
+             theLink.style.backgroundColor = 'green';
+         navs.append(theLink);
+           }
+           addLinkToNav();
 
 //exercise 12
+// sol 1 
+/*function removeSearch(){
+    document.getElementById("Search").innerHTML = "";
+}*/
+sol2
+function removeSearchButton() {
+    let searchBtn = document.querySelector('.text-muted svg');
+    searchBtn.style.display = 'none';
+   }
+   removeSearchButton();
 
 
 
 //exercise 13
  function changeColor () {
      let selectedE = document.querySelector('.jumbotron');
-     selectedE.style.backgroundColor = 'green';
+     selectedE.classList.remove('bg-dark');
+     selectedE.classList.add('bg-success');
 
  }
  window.onload = changeColor();
@@ -31,16 +52,38 @@
  window.onload = changeTitle();
 
  //exercise 15
+ /*let titleForBlog  = document.querySelectorAll('.blog-post-title');
+ function changeSize() {
+ for(i=0 ; i < titleForBlog.length; i++) {
+     titleForBlog[i].classList.add('col-6');
+ }
+}*/
+function columnSize(){
+    let element = document.querySelectorAll(".featured");
+    for (let i = 0; i<element.length; i++)
+    element[i].classList.remove("col-md-6");
+    element[i].classList.add("col-4");
+}
 
 
  //exercise 16
- window.onload = function removeLinks (){
-     let lis = document.querySelector('.second')
+ sol 1
+ /*window.onload = function removeLinks (){
+     let lis = document.querySelector('.second li')
      let newLi = document.querySelectorAll('.new')
      for(let i = 0; i< newLi.length; i++){
          lis.removeChild(newLi[i])
      }
- }
+ }*/
+ window.onload = function removeLinksElswhere(){
+    let list = document.querySelectorAll("ol .new")
+    for (let i = 0; i<list.length;i++){
+        list.innerHTML = ""
+    }
+    
+
+}
+
 
  // exercise 17
 
